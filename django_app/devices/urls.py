969191a0a6cfcb7fd255d66b15201171f47fb03d
Jpoker01 <jiripokorny455@gmail.com>
2026-08-04 01:36:25 +0200

    clean up, api working for measurements

--- django_app/devices/urls.py
+++ django_app/devices/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DeviceListView, DeviceDetailView
+from .views import DeviceListView, DeviceDetailView, DeviceCommandView
 
 urlpatterns = [
     path("devices/", DeviceListView.as_view(), name="device-list"),
     path("devices/<int:pk>/", DeviceDetailView.as_view(), name="device-detail"),
+    path("devices/<int:pk>/command/", DeviceCommandView.as_view(), name="device-command"),
+    path('api/', include('measurements.urls'))
 ]
---
+++ django_app/measurements/urls.py
@@ -0,0 +1,3 @@
+from django.urls import path
+from .views import MeasurementListView
+urlpatterns = [path("measurements/", MeasurementListView.as_view(), name="measurement-list")]
