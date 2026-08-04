f2ddaa36164d442e4c887ff1f07894e9af90de72
Jpoker01 <jiripokorny455@gmail.com>
2026-08-04 10:49:19 +0200

    added password authentication to common/broker.py

--- services/common/broker.py
+++ services/common/broker.py
@@ -6,6 +6,8 @@ MQTT_USERNAME = os.getenv("MQTT_USERNAME")
 MQTT_PASSWORD = os.getenv("MQTT_PASSWORD")
 
 def connect_to_broker(client, host, port):
+    if MQTT_USERNAME and MQTT_PASSWORD:
+        client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
     try:
         client.connect(host, port, keepalive=60)
     except OSError as exc:
