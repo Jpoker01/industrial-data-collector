7fc062dce593f77d55a2137d133504e5dbaa08dd
Jpoker01 <jiripokorny455@gmail.com>
2026-08-05 19:40:24 +0200

    setup.sh and final edits complete

--- setup.sh
+++ setup.sh
@@ -10,20 +10,17 @@ if [ -f .env ]; then
     set +a
 fi
 
-echo "Generating Mosquitto password file..."
-# (You might need to prefix this rm with sudo just in case it was created by root previously)
-sudo rm -f "$CONFIG_DIR/passwd"
+rm -f "$CONFIG_DIR/passwd" 2>/dev/null || sudo rm -f "$CONFIG_DIR/passwd"
 
-# Run as 1883 so it has permission to write in the mosquitto/config directory
-docker run --rm --user 1883:1883 \
+docker run --rm \
     -v "$(pwd)/$CONFIG_DIR:/mosquitto/config" eclipse-mosquitto:2 \
     mosquitto_passwd -c -b /mosquitto/config/passwd publisher "$MQTT_PUBLISHER_PASSWORD"
-
-docker run --rm --user 1883:1883 \
+ 
+docker run --rm \
     -v "$(pwd)/$CONFIG_DIR:/mosquitto/config" eclipse-mosquitto:2 \
     mosquitto_passwd -b /mosquitto/config/passwd ingest "$MQTT_INGEST_PASSWORD"
-
-docker run --rm --user 1883:1883 \
+ 
+docker run --rm \
     -v "$(pwd)/$CONFIG_DIR:/mosquitto/config" eclipse-mosquitto:2 \
     mosquitto_passwd -b /mosquitto/config/passwd django "$MQTT_DJANGO_PASSWORD"
 
